b58d3d36c03b3c2383fb55538e216c146c134d8c
kwesbian <kwechristian@gmail.com>
2024-07-08 18:03:41 +0300

    Update speedDetect.js

--- speedDetect.js
+++ speedDetect.js
@@ -1,8 +1,10 @@
+speedDetector()
+
 function speedDetector(){
     let speed = prompt("Enter the speed of the car (in km/h): ");
     speed = parseInt(speed);
 
-    if (speed < 70){
+    if (speed >= 0 && speed < 70){
         console.log("Ok")
     }
     else {
@@ -12,8 +14,8 @@ function speedDetector(){
         if (demeritPoints > 12){
             console.log("License Suspended")
         }
-        else{
-        console.log("You have ${demeritPoints} demetit points");
+        else {
+        console.log('You have ${demeritPoints} demerit points');
         }
     }
 }
