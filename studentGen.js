e5d1cf51c3b786c289edc28ec421b07def568486
kwesbian <kwechristian@gmail.com>
2024-07-08 18:05:52 +0300

    Update studentGen.js

--- studentGen.js
+++ studentGen.js
@@ -1,9 +1,11 @@
+studentGradeGeneraor()
+
 function studentGradeGeneraor(){
     let mark = prompt("Enter your marks (between 0 and 100): ");
-        if (mark > 79){
+        if (mark >= 80 && mark < 100){
             console.log("A")
         }
-        else if (mark >=60 && mark <= 79){
+        else if (mark >=60 && mark < 80){
             console.log("B")
         }
         else if (mark >=59 && mark <= 49){
@@ -12,7 +14,10 @@ function studentGradeGeneraor(){
         else if (mark >= 40 && mark <= 49){
             console.log("D")
         }
-        else (mark < 40){
+        else if (mark > 0 && mark < 40){
             console.log("E")
         }
-}
+        else {
+            console.log("Invalid")
+        }
+    }
